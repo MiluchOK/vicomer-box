e461531c1c3c96cabb4cfdce4dcddd59cf5ff671
MiluchOK <alexeymilyukov@Alexeys-MacBook-Pro.local>
2017-10-11 11:59:55 -0700

    Testing out

--- app/src/App.js
+++ app/src/App.js
@@ -12,20 +12,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to resin-electronjs-react</h2>
-        </div>
-        <div className="App-intro">
-          <p>
-          To get started, edit <code>src/App.js</code> and save to reload. You have access to all node apis. Here is a list of files in this directory.
-          </p>
-          {
-            files.map((file, key) => {
-              return <pre key={key}>{file}</pre>;
-            })
-          }
-        </div>
+        Shit
       </div>
     );
   }
