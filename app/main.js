f953b2355508e701e290aa119f6f1aa6c8e7c54c
MiluchOK <alexeymilyukov@Alexeys-MacBook-Pro.local>
2017-10-11 17:51:40 -0700

    Reverting back resolution

--- app/main.js
+++ app/main.js
@@ -46,8 +46,8 @@ if (process.env.NODE_ENV === 'development') {
 app.on('ready', () => {
   // here we actually configure the behavour of electronJS
   const window = new BrowserWindow({
-    // width: 1820,
-    // height: 980,
+    width: electronConfig.URL_LAUNCHER_WIDTH,
+    height: electronConfig.URL_LAUNCHER_HEIGHT,
     frame: false,
     fullscreen: true,
     // title: electronConfig.URL_LAUNCHER_TITLE,
